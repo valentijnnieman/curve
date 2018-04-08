32c102ad48a1de9334c8188d4edac850129f901e
Valentijn Nieman <valentijnnieman@gmail.com>
2018-04-08 21:00:10 +0200

    Remove service worker for now

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
+// remove service worker for now, as the app will be updated regularly
+// registerServiceWorker();
