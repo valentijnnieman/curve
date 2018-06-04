7d16682d67d3808f66b8236a6c6035ebcfe01a2d
Valentijn Nieman <valentijnnieman@gmail.com>
2018-06-04 11:50:06 +0200

    Removing .env.development files

--- server/index.js
+++ server/index.js
@@ -32,12 +32,6 @@ if (cluster.isMaster) {
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
-  // Answer API requests.
-  app.get("/api", function(req, res) {
-    res.set("Content-Type", "application/json");
-    res.send('{"message":"Hello from the custom server!"}');
-  });
-
   // Create synth route
   app.post("/synth/create", require("./controllers/synth").create);
 
