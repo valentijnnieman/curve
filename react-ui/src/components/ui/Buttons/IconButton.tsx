9233e8ed0e09557105cce6f8f51a08d224ca3826
Valentijn Nieman <valentijnnieman@gmail.com>
2019-08-22 14:11:47 -0400

    Basic tooltip for redesign

--- react-ui/src/components/ui/Buttons/IconButton.tsx
+++ react-ui/src/components/ui/Buttons/IconButton.tsx
@@ -1,11 +1,49 @@
 import * as React from "react";
 import "./Button.css";
 
-export default (props: any) => {
-  const buttonClass = `button floating-action-button ${props.className}`;
-  return (
-    <div className={buttonClass} onClick={props.onClick}>
-      {props.children}
-    </div>
-  );
-};
+interface IconButtonProps {
+  tooltip?: string;
+  tooltipPosition?: string;
+  className?: string;
+  tooltipStyles?: object;
+  onClick?: any;
+}
+
+interface IconButtonState {
+  tooltipEnabled: boolean;
+}
+
+export default class IconButton extends React.Component<
+  IconButtonProps,
+  IconButtonState
+> {
+  constructor(props: IconButtonProps) {
+    super(props);
+    this.state = {
+      tooltipEnabled: false
+    };
+  }
+
+  render() {
+    const buttonClass = `button floating-action-button ${this.props.className}`;
+    return (
+      <div>
+        <span
+          className={`tooltip ${
+            this.state.tooltipEnabled ? "" : "tooltip--hidden"
+          }`}
+        >
+          {this.props.tooltip}
+        </span>
+        <div
+          className={buttonClass}
+          onMouseEnter={() => this.setState({ tooltipEnabled: true })}
+          onMouseLeave={() => this.setState({ tooltipEnabled: false })}
+          onClick={this.props.onClick}
+        >
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
