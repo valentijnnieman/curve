d71f947f609209ffae4cde8f6070a212a13c7a7b
valentijnnieman <valentijnnieman@gmail.com>
2023-03-29 15:03:58 +0200

    remove redux dev tools for now

--- react-ui/src/store.ts
+++ react-ui/src/store.ts
@@ -2,11 +2,10 @@ import thunkMiddleware from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/index";
 import { StoreState } from "./types/storeState";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = createStore<StoreState, any, any, any>(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunkMiddleware))
+  applyMiddleware(thunkMiddleware)
 );
 
 export default store;
